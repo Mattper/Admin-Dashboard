5d2d1ea52f7fe014f2ce33b5dc90eab5808e8fd8
Nimesh Perera <mattperdevstuff@gmail.com>
2022-11-04 05:40:49 +0530

    Dashboard implementation

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -64,6 +64,7 @@ const LineChart= () =>{
             axisLeft={{
                 orient: 'left',
                 tickSize: 5,
+                tickValues: 5,
                 tickPadding: 5,
                 tickRotation: 0,
                 legend: 'count',
